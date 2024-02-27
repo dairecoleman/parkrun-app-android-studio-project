fa479ec410472edb427f98a08c4cd78181a95556
daire <dairecoleman@gmail.com>
2024-02-27 20:48:55 +0000

    initial project commit

---
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.myfirstapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.myfirstapplication.ui.theme.MyFirstApplicationTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyFirstApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Daire")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Surface(color = Color.Magenta) {
+        Text(
+            text = "Hi my name is $name!",
+            modifier = modifier.padding(24.dp)
+        )
+    }
+}
+
+@Preview(showBackground = false)
+@Composable
+fun GreetingPreview() {
+    MyFirstApplicationTheme {
+        Greeting("Daire")
+    }
+}
